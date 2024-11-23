a5dacc729f0635c741fe7e0212b8ccf8bf221124
DenGian <DenGian@users.noreply.github.com>
2024-11-23 19:55:08 +0100

    WIP

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,26 @@
-import { Stack } from "expo-router";
+import * as SecureStore from 'expo-secure-store';
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
+import { Slot } from 'expo-router';
 
-const RootLayout = () => {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+const tokenCache = {
+    async getToken(key: string) {
+        return SecureStore.getItemAsync(key);
+    },
+    async saveToken(key: string, value: string) {
+        return SecureStore.setItemAsync(key, value);
+    },
+};
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+const RootLayoutNav = () => {
+    return (
+        <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey!}>
+            <ClerkLoaded>
+                <Slot />
+            </ClerkLoaded>
+        </ClerkProvider>
+    );
 }
 
-export default RootLayout;
+export default RootLayoutNav;
