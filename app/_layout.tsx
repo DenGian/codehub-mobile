6bda5ae7ad8a0811f714112e85faccdf0d793d72
DenGian <DenGian@users.noreply.github.com>
2024-11-24 21:15:18 +0100

    Updated layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,46 +1,61 @@
-import * as SecureStore from 'expo-secure-store'
-import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
-import { Slot } from 'expo-router'
+import {ClerkProvider, useAuth} from '@clerk/clerk-expo';
+import {Slot, useRouter, useSegments} from 'expo-router';
+import {useEffect} from 'react';
+import * as SecureStore from 'expo-secure-store';
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!CLERK_PUBLISHABLE_KEY) {
+    throw new Error(
+        "Missing publishable key. Set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in .env"
+    )
+}
+
+const InitialLayout = () => {
+    const {isLoaded, isSignedIn} = useAuth();
+    const segments = useSegments();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!isLoaded) return;
+
+        const inTabsGroup = segments[0] === '(auth)';
+
+        console.log('User changed: ', isSignedIn);
+
+        if (isSignedIn && !inTabsGroup) {
+            router.replace('/home');
+        } else if (!isSignedIn) {
+            router.replace('/login');
+        }
+    }, [isSignedIn]);
+
+    return <Slot/>;
+};
 
 const tokenCache = {
     async getToken(key: string) {
         try {
-            const item = await SecureStore.getItemAsync(key)
-            if (item) {
-                console.log(`${key} was used 🔐 \n`)
-            } else {
-                console.log('No values stored under key: ' + key)
-            }
-            return item
-        } catch (error) {
-            console.error('SecureStore get item error: ', error)
-            await SecureStore.deleteItemAsync(key)
-            return null
+            return SecureStore.getItemAsync(key);
+        } catch (err) {
+            return null;
         }
     },
     async saveToken(key: string, value: string) {
         try {
-            return SecureStore.setItemAsync(key, value)
+            return SecureStore.setItemAsync(key, value);
         } catch (err) {
-            return
+            return;
         }
     },
-}
+};
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
-
-if (!publishableKey) {
-    throw new Error(
-        'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
-    )
-}
-
-function RootLayoutNav() {
+const RootLayout = () => {
     return (
-        <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-            <ClerkLoaded>
-                <Slot />
-            </ClerkLoaded>
+        <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache}>
+            <InitialLayout/>
         </ClerkProvider>
-    )
-}
+    );
+};
+
+export default RootLayout;
