0a0e1f069bcd4d0f9142ffa732d4a0fbb7618630
DenGian <DenGian@users.noreply.github.com>
2024-11-24 21:16:01 +0100

    WIP

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,11 @@
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
-import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import {ActivityIndicator, View} from 'react-native';
 
-export default function HomePage() {
-    const { user } = useUser();
-
+const StartPage = () => {
     return (
-        <View>
-            <SignedIn>
-                <Text>Hello, {user?.emailAddresses[0].emailAddress}</Text>
-            </SignedIn>
-            <SignedOut>
-                <Link href={{ pathname: '../sign-in' }}>
-                    <Text>Sign In</Text>
-                </Link>
-                <Link href={{ pathname: '../sign-up' }}>
-                    <Text>Sign Up</Text>
-                </Link>
-            </SignedOut>
+        <View style={{flex: 1, justifyContent: 'center'}}>
+            <ActivityIndicator size="large" color="#0000ff"/>
         </View>
     );
-}
+};
+
+export default StartPage;
