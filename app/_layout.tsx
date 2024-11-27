752c030bcec0e92c1783bf00d3598e3bed21f8a1
DenGian <DenGian@users.noreply.github.com>
2024-11-27 14:07:22 +0100

    Removed env variable check to utility function

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,7 @@ import {ClerkProvider} from '@clerk/clerk-expo';
 import {Slot} from 'expo-router';
 import useAuthRedirect from '@/hooks/auth/useAuthRedirect';
 import useTokenCache from '@/hooks/auth/useTokenCache';
-
-const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
-
-if (!CLERK_PUBLISHABLE_KEY) {
-    throw new Error("Set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in .env");
-}
+import getClerkPublishableKey from '@/utils/getClerkPublishableKey';
 
 const InitialLayout = () => {
     useAuthRedirect();
@@ -16,9 +11,10 @@ const InitialLayout = () => {
 
 const RootLayoutNav = () => {
     const tokenCache = useTokenCache();
+    const CLERK_PUBLISHABLE_KEY = getClerkPublishableKey();
 
     return (
-        <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache}>
+        <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY} tokenCache={tokenCache}>
             <InitialLayout/>
         </ClerkProvider>
     );
