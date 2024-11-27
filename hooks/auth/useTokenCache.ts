750d8279943ea454307528e9b962921e625e1126
DenGian <DenGian@users.noreply.github.com>
2024-11-27 14:03:55 +0100

    Made TokenCache hook

---
+++ hooks/auth/useTokenCache.ts
@@ -0,0 +1,23 @@
+import * as SecureStore from 'expo-secure-store';
+import {TokenCache} from '@/types/TokenCache';
+
+const useTokenCache = (): TokenCache => {
+    return {
+        async getToken(key: string) {
+            try {
+                return SecureStore.getItemAsync(key);
+            } catch (err) {
+                return null;
+            }
+        },
+        async saveToken(key: string, value: string) {
+            try {
+                return SecureStore.setItemAsync(key, value);
+            } catch (err) {
+                return;
+            }
+        }
+    };
+};
+
+export default useTokenCache;
