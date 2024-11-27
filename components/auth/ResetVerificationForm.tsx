b5ec5b85e5cd3be0fc22c7a5a558cad833e2f8f0
DenGian <DenGian@users.noreply.github.com>
2024-11-27 17:25:46 +0100

    Created ResetVerificationForm component

---
+++ components/auth/ResetVerificationForm.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {View, TextInput} from 'react-native';
+import resetStyles from '@/styles/auth/resetStyles';
+import PrimaryButton from "@/components/ui/PrimaryButton";
+
+interface ResetVerificationFormProps {
+    code: string;
+    setCode: (text: string) => void;
+    password: string;
+    setPassword: (text: string) => void;
+    onReset: () => void;
+}
+
+const ResetVerificationForm: React.FC<ResetVerificationFormProps> = (
+    {code, setCode, password, setPassword, onReset}) => {
+    return (
+        <View>
+            <TextInput
+                value={code}
+                placeholder="Code..."
+                placeholderTextColor="black"
+                style={resetStyles.inputField}
+                onChangeText={setCode}
+            />
+            <TextInput
+                placeholder="New password"
+                placeholderTextColor="black"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+                style={resetStyles.inputField}
+            />
+            <PrimaryButton onPress={onReset} title="Set new Password" color={'#6c47ff'}/>
+        </View>
+    );
+};
+
+export default ResetVerificationForm;
