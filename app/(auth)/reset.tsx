f9419b69544bc5563ddc1f6a34db2b836d1d06c5
DenGian <DenGian@users.noreply.github.com>
2024-11-27 17:28:48 +0100

    Implemented usePasswordReset hook

--- app/(auth)/reset.tsx
+++ app/(auth)/reset.tsx
@@ -1,44 +1,23 @@
 import {View} from 'react-native';
-import React, {useState} from 'react';
+import React from 'react';
 import {Stack} from 'expo-router';
-import {useSignIn} from '@clerk/clerk-expo';
 import resetStyles from '@/styles/auth/resetStyles';
 import ResetForm from '@/components/auth/ResetForm';
 import ResetVerificationForm from '@/components/auth/ResetVerificationForm';
+import usePasswordReset from '@/hooks/auth/usePasswordReset';
 
 const PwReset = () => {
-    const [emailAddress, setEmailAddress] = useState('');
-    const [password, setPassword] = useState('');
-    const [code, setCode] = useState('');
-    const [successfulCreation, setSuccessfulCreation] = useState(false);
-    const {signIn, setActive} = useSignIn();
-
-    const onRequestReset = async () => {
-        try {
-            await signIn!.create({
-                strategy: 'reset_password_email_code',
-                identifier: emailAddress,
-            });
-            setSuccessfulCreation(true);
-        } catch (err: any) {
-            alert(err.errors[0].message);
-        }
-    };
-
-    const onReset = async () => {
-        try {
-            const result = await signIn!.attemptFirstFactor({
-                strategy: 'reset_password_email_code',
-                code,
-                password,
-            });
-            console.log(result);
-            alert('Password reset successfully');
-            await setActive!({session: result.createdSessionId});
-        } catch (err: any) {
-            alert(err.errors[0].message);
-        }
-    };
+    const {
+        emailAddress,
+        setEmailAddress,
+        password,
+        setPassword,
+        code,
+        setCode,
+        successfulCreation,
+        onRequestReset,
+        onReset,
+    } = usePasswordReset();
 
     return (
         <View style={resetStyles.container}>
