2fe97adb8f12a3e7b8cf50851beae1528c98d993
DenGian <DenGian@users.noreply.github.com>
2024-11-27 14:01:36 +0100

    Working on seperating concerns

---
+++ hooks/auth/useAuthRedirect.ts
@@ -0,0 +1,23 @@
+import {useEffect} from 'react';
+import {useRouter, useSegments} from 'expo-router';
+import {useAuth} from '@clerk/clerk-expo';
+
+const useAuthRedirect = () => {
+    const {isLoaded, isSignedIn} = useAuth();
+    const segments = useSegments();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!isLoaded) return;
+
+        const inAuthGroup = segments[0] === '(auth)';
+
+        if (isSignedIn && inAuthGroup) {
+            router.replace("/(routes)/(tabs)/home");
+        } else if (!isSignedIn && !inAuthGroup) {
+            router.replace('/(auth)/login');
+        }
+    }, [isLoaded, isSignedIn, segments]);
+};
+
+export default useAuthRedirect;
