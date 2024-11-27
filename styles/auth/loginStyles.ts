42025693eba58e6434f3fd76e3d51cb57687a646
DenGian <DenGian@users.noreply.github.com>
2024-11-27 14:27:38 +0100

    Add styles folder + add login styles

---
+++ styles/auth/loginStyles.ts
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+
+const loginStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 20
+    },
+    inputField: {
+        marginVertical: 4,
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#6c47ff',
+        borderRadius: 4,
+        padding: 10,
+        backgroundColor: '#fff'
+    },
+    button: {
+        margin: 8,
+        alignItems: 'center'
+    }
+});
+
+export default loginStyles;
