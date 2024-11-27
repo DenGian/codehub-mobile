2d87af7366c826ac5a3936e10a00974b15473cc4
DenGian <DenGian@users.noreply.github.com>
2024-11-27 17:29:07 +0100

    Created passwordReset hook

---
+++ hooks/auth/usePasswordReset.ts
@@ -0,0 +1,51 @@
+import {useState} from 'react';
+import {useSignIn} from '@clerk/clerk-expo';
+
+const usePasswordReset = () => {
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [code, setCode] = useState('');
+    const [successfulCreation, setSuccessfulCreation] = useState(false);
+    const {signIn, setActive} = useSignIn();
+
+    const onRequestReset = async () => {
+        try {
+            await signIn!.create({
+                strategy: 'reset_password_email_code',
+                identifier: emailAddress,
+            });
+            setSuccessfulCreation(true);
+        } catch (err: any) {
+            alert(err.errors[0].message);
+        }
+    };
+
+    const onReset = async () => {
+        try {
+            const result = await signIn!.attemptFirstFactor({
+                strategy: 'reset_password_email_code',
+                code,
+                password,
+            });
+            console.log(result);
+            alert('Password reset successfully');
+            await setActive!({session: result.createdSessionId});
+        } catch (err: any) {
+            alert(err.errors[0].message);
+        }
+    };
+
+    return {
+        emailAddress,
+        setEmailAddress,
+        password,
+        setPassword,
+        code,
+        setCode,
+        successfulCreation,
+        onRequestReset,
+        onReset,
+    };
+};
+
+export default usePasswordReset;
