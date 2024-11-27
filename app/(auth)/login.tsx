a56434f164052fb5cbdd615bfb02c288b59a2a0f
DenGian <DenGian@users.noreply.github.com>
2024-11-27 14:30:14 +0100

    extract the form handling logic into a custom hook

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,36 +1,19 @@
-import {useSignIn} from '@clerk/clerk-expo';
 import {Link} from 'expo-router';
-import React, {useState} from 'react';
+import React from 'react';
 import {View, TextInput, Button, Pressable, Text} from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
 import loginStyles from '@/styles/auth/loginStyles';
+import useLoginForm from '@/hooks/auth/useLoginForm';
 
 const login = () => {
-    const {signIn, setActive, isLoaded} = useSignIn();
-
-    const [emailAddress, setEmailAddress] = useState('');
-    const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-
-    const onSignInPress = async () => {
-        if (!isLoaded) {
-            return;
-        }
-        setLoading(true);
-        try {
-            const completeSignIn = await signIn.create({
-                identifier: emailAddress,
-                password
-            });
-
-            // This indicates the user is signed in
-            await setActive({session: completeSignIn.createdSessionId});
-        } catch (err: any) {
-            alert(err.errors[0].message);
-        } finally {
-            setLoading(false);
-        }
-    };
+    const {
+        emailAddress,
+        setEmailAddress,
+        password,
+        setPassword,
+        loading,
+        onSignInPress
+    } = useLoginForm();
 
     return (
         <View style={loginStyles.container}>
