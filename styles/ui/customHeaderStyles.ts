b8409361e9d93c0ec3c356a3278325ef98ea9105
DenGian <DenGian@users.noreply.github.com>
2024-11-27 21:53:14 +0100

    Add Header styles

---
+++ styles/ui/customHeaderStyles.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const customHeaderStyles = StyleSheet.create({
+    headerContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        padding: 10,
+        backgroundColor: '#6c47ff',
+    },
+    headerText: {
+        color: '#fff',
+        fontSize: 16,
+    },
+});
+
+export default customHeaderStyles;
