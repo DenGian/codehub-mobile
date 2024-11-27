a7dc1c9ce8442ccc3e8231c39367c543669edac1
DenGian <DenGian@users.noreply.github.com>
2024-11-27 15:14:53 +0100

    Implemented useRegisterFormHook

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,57 +1,25 @@
 import {Button, TextInput, View} from 'react-native';
-import {useSignUp} from '@clerk/clerk-expo';
-import React, {useState} from 'react';
+import React from 'react';
 import Spinner from 'react-native-loading-spinner-overlay';
 import {Stack} from 'expo-router';
 import registerStyles from '@/styles/auth/registerStyles';
+import useRegisterForm from '@/hooks/auth/useRegisterForm';
 
 const Register = () => {
-    const {isLoaded, signUp, setActive} = useSignUp();
-    const [emailAddress, setEmailAddress] = useState('');
-    const [password, setPassword] = useState('');
-    const [username, setUsername] = useState('');
-    const [pendingVerification, setPendingVerification] = useState(false);
-    const [code, setCode] = useState('');
-    const [loading, setLoading] = useState(false);
-
-    const onSignUpPress = async () => {
-        if (!isLoaded) {
-            return;
-        }
-        setLoading(true);
-
-        try {
-            await signUp.create({
-                emailAddress,
-                password,
-                username,
-            });
-
-            await signUp.prepareEmailAddressVerification({strategy: 'email_code'});
-
-            setPendingVerification(true);
-        } catch (err: any) {
-            alert(err.errors[0].message);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const onPressVerify = async () => {
-        if (!isLoaded) {
-            return;
-        }
-        setLoading(true);
-
-        try {
-            const completeSignUp = await signUp.attemptEmailAddressVerification({code});
-            await setActive({session: completeSignUp.createdSessionId});
-        } catch (err: any) {
-            alert(err.errors[0].message);
-        } finally {
-            setLoading(false);
-        }
-    };
+    const {
+        emailAddress,
+        setEmailAddress,
+        password,
+        setPassword,
+        username,
+        setUsername,
+        pendingVerification,
+        code,
+        setCode,
+        loading,
+        onSignUpPress,
+        onPressVerify
+    } = useRegisterForm();
 
     return (
         <View style={registerStyles.container}>
