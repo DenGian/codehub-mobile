f70e6f14ff27f93b16335ca256c85e4cad5e7031
DenGian <DenGian@users.noreply.github.com>
2024-11-27 19:27:57 +0100

    Further seperating concerns

--- app/(routes)/(tabs)/profile/index.tsx
+++ app/(routes)/(tabs)/profile/index.tsx
@@ -1,43 +1,21 @@
 import {View} from 'react-native';
-import {useState} from 'react';
-import {useUser} from '@clerk/clerk-expo';
 import profileStyles from '@/styles/routes/tabs/profileStyles';
-import InputField from '@/components/ui/InputField';
 import PrimaryButton from '@/components/ui/PrimaryButton';
 import GreetingText from '@/components/ui/GreetingText';
+import useUpdateUser from '@/hooks/route/tabs/useUpdateUser';
+import UserInputFields from '@/components/ui/UserInputFields';
 
 const index = () => {
-    const {user} = useUser();
-    const [firstName, setFirstName] = useState(user?.firstName);
-    const [lastName, setLastName] = useState(user?.lastName);
-    const [loading, setLoading] = useState(false);
-
-    const onSaveUser = async () => {
-        setLoading(true);
-        try {
-            await user?.update({
-                firstName: firstName!,
-                lastName: lastName!,
-            });
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setLoading(false);
-        }
-    };
+    const {firstName, setFirstName, lastName, setLastName, loading, onSaveUser} = useUpdateUser();
 
     return (
         <View style={profileStyles.container}>
-            <GreetingText firstName={user?.firstName || ''} lastName={user?.lastName || ''}/>
-            <InputField
-                placeholder="First Name"
-                value={firstName || ''}
-                onChangeText={setFirstName}
-            />
-            <InputField
-                placeholder="Last Name"
-                value={lastName || ''}
-                onChangeText={setLastName}
+            <GreetingText firstName={firstName || ''} lastName={lastName || ''}/>
+            <UserInputFields
+                firstName={firstName || ''}
+                setFirstName={setFirstName}
+                lastName={lastName || ''}
+                setLastName={setLastName}
             />
             <PrimaryButton onPress={onSaveUser} title="Update account" color="#6c47ff" loading={loading}/>
         </View>
