0744d4f45ac7e04fe7f2f65ab0df03d3e819c5d7
DenGian <DenGian@users.noreply.github.com>
2024-11-27 14:00:55 +0100

    Removed hook and imported it from hooks folder

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,8 @@
-import {useEffect} from 'react';
 import * as SecureStore from 'expo-secure-store';
-import {ClerkProvider, useAuth} from '@clerk/clerk-expo';
-import {Slot, useRouter, useSegments} from 'expo-router';
-import {TokenCache} from "@/types/TokenCache";
+import {ClerkProvider} from '@clerk/clerk-expo';
+import {Slot} from 'expo-router';
+import {TokenCache} from '@/types/TokenCache';
+import useAuthRedirect from '@/hooks/auth/useAuthRedirect';
 
 const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
@@ -29,24 +29,7 @@ const tokenCache: TokenCache = {
 };
 
 const InitialLayout = () => {
-    const {isLoaded, isSignedIn} = useAuth();
-    const segments = useSegments();
-    const router = useRouter();
-
-    // If the user is signed in, redirect them to the home page
-    // If the user is not signed in, redirect them to the login page
-    useEffect(() => {
-        if (!isLoaded) return;
-
-        const inAuthGroup = segments[0] === '(auth)';
-
-        if (isSignedIn && inAuthGroup) {
-            router.replace("/(routes)/(tabs)/home");
-        } else if (!isSignedIn && !inAuthGroup) {
-            router.replace('/(auth)/login');
-        }
-    }, [isSignedIn]);
-
+    useAuthRedirect();
     return <Slot/>;
 };
 
