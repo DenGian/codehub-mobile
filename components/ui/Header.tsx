5ab01eeed7ab80883fd47b7bd79f2c302fcbbd55
DenGian <DenGian@users.noreply.github.com>
2024-11-27 21:52:54 +0100

    Seperated styles from HeaderComponent

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,29 +1,17 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
 import {useUser} from '@clerk/clerk-expo';
+import customHeaderStyles from '@/styles/ui/customHeaderStyles';
 
 const CustomHeader: React.FC = () => {
     const {user} = useUser();
     const displayName = user?.firstName || user?.username;
 
     return (
-        <View style={styles.headerContainer}>
-            <Text style={styles.headerText}>Welcome, {displayName}</Text>
+        <View style={customHeaderStyles.headerContainer}>
+            <Text style={customHeaderStyles.headerText}>Welcome, {displayName}</Text>
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    headerContainer: {
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        padding: 10,
-        backgroundColor: '#6c47ff',
-    },
-    headerText: {
-        color: '#fff',
-        fontSize: 16,
-    },
-});
 
 export default CustomHeader;
