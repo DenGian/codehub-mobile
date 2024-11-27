35ef8b887607b0ddf66b967466bf4452d4882f42
DenGian <DenGian@users.noreply.github.com>
2024-11-27 14:19:28 +0100

    Seperating concerns

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,37 +1,8 @@
 import React from 'react';
-import {Stack} from 'expo-router';
+import AuthPages from '@/screens/AuthPages';
 
 const AuthLayout = () => {
-    return (
-        <Stack
-            screenOptions={{
-                headerStyle: {
-                    backgroundColor: '#6c47ff'
-                },
-                headerTintColor: '#fff',
-                headerBackTitle: 'Back'
-            }}
-        >
-            <Stack.Screen
-                name="login"
-                options={{
-                    headerTitle: 'Code-Hub'
-                }}
-            />
-            <Stack.Screen
-                name="register"
-                options={{
-                    headerTitle: 'Create Account'
-                }}
-            />
-            <Stack.Screen
-                name="reset"
-                options={{
-                    headerTitle: 'Reset Password'
-                }}
-            />
-        </Stack>
-    );
+    return <AuthPages/>;
 };
 
 export default AuthLayout;
