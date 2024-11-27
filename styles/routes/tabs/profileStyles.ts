043b1118ced7c61e32adf8515b41e02d75b74871
DenGian <DenGian@users.noreply.github.com>
2024-11-27 19:10:48 +0100

    Add styles for profile page

---
+++ styles/routes/tabs/profileStyles.ts
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+const profileStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 40,
+    },
+    inputField: {
+        marginVertical: 4,
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#6c47ff',
+        borderRadius: 4,
+        padding: 10,
+        backgroundColor: '#fff',
+    },
+});
+
+export default profileStyles;
