317bfd108851b08851e3bf1f3cd620b08d7c4630
DenGian <DenGian@users.noreply.github.com>
2024-11-27 14:49:55 +0100

    Extracted login form

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import {View} from 'react-native';
 import loginStyles from '@/styles/auth/loginStyles';
 import useLoginForm from '@/hooks/auth/useLoginForm';
-import LoginInput from '@/components/auth/LoginInput';
 import LoginButton from '@/components/auth/LoginButton';
 import LoadingSpinner from '@/components/ui/LoadingSpinner';
-import PrimaryButton from '@/components/ui/PrimaryButton';
+import LoginForm from '@/components/auth/LoginForm';
 
 const login = () => {
     const {
@@ -21,19 +20,13 @@ const login = () => {
         <View style={loginStyles.container}>
             <LoadingSpinner visible={loading}/>
 
-            <LoginInput
-                placeholder="code@hub.dev"
-                value={emailAddress}
-                onChangeText={setEmailAddress}
+            <LoginForm
+                emailAddress={emailAddress}
+                setEmailAddress={setEmailAddress}
+                password={password}
+                setPassword={setPassword}
+                onSignInPress={onSignInPress}
             />
-            <LoginInput
-                placeholder="password"
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry
-            />
-
-            <PrimaryButton onPress={onSignInPress} title="Login"/>
 
             <LoginButton href="/reset" text="Forgot password?"/>
             <LoginButton href="/register" text="Create Account"/>
