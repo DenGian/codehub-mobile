35a45a6c000a63f952f3e60a3aebe61cfddb4624
DenGian <DenGian@users.noreply.github.com>
2024-11-28 01:42:58 +0100

    Add AsyncStorage for favorites

---
+++ hooks/storage/useFavorites.ts
@@ -0,0 +1,39 @@
+import {useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const useFavorites = () => {
+    const [favorites, setFavorites] = useState<number[]>([]);
+
+    useEffect(() => {
+        const loadFavorites = async () => {
+            try {
+                const storedFavorites = await AsyncStorage.getItem('favorites');
+                if (storedFavorites) {
+                    setFavorites(JSON.parse(storedFavorites));
+                }
+            } catch (error) {
+                console.error('Failed to load favorites from AsyncStorage:', error);
+            }
+        };
+
+        loadFavorites().then(() => {
+        });
+    }, []);
+
+    const toggleFavorite = async (id: number) => {
+        try {
+            let updatedFavorites = [...favorites];
+            if (updatedFavorites.includes(id)) {
+                updatedFavorites = updatedFavorites.filter((favorite) => favorite !== id);
+            } else {
+                updatedFavorites.push(id);
+            }
+            setFavorites(updatedFavorites);
+            await AsyncStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+        } catch (error) {
+            console.error('Failed to update favorites in AsyncStorage:', error);
+        }
+    };
+
+    return {favorites, toggleFavorite};
+};
