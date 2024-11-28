e474ebcbedf9c30fd5833fc043863807f81d065c
DenGian <DenGian@users.noreply.github.com>
2024-11-28 16:59:15 +0100

    Updated UI

--- app/(routes)/(modals)/home/details/[id].tsx
+++ app/(routes)/(modals)/home/details/[id].tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import {useLocalSearchParams, useRouter} from 'expo-router';
-import {Button, View} from 'react-native';
+import {View} from 'react-native';
+import {useLocalSearchParams} from 'expo-router';
 import ResourceDetailsScreen from "@/screens/details";
 
 const ResourceDetailsPage: React.FC = () => {
     const {id} = useLocalSearchParams<{ id: string }>();  // Get the dynamic parameter
-    const router = useRouter();
 
     return (
         <View style={{flex: 1}}>
-            <Button title="Back" onPress={() => router.back()}/>
             <ResourceDetailsScreen id={id}/> {/* Pass the id parameter correctly */}
         </View>
     );
