dd6a5fabd513b3e1702da648f6f5aa9bb9d86c45
DenGian <DenGian@users.noreply.github.com>
2024-11-28 22:27:58 +0100

    Add mapStyles

---
+++ styles/routes/tabs/home/mapStyles.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+const mapStyles = StyleSheet.create({
+    map: {
+        height: 200,
+        marginVertical: 10,
+    },
+});
+
+export default mapStyles;
