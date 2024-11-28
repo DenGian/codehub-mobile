1975e630c46c9b4d86fa599f26a1311f7b619fca
DenGian <DenGian@users.noreply.github.com>
2024-11-28 22:41:11 +0100

    Add ResourceDetails component

---
+++ components/ui/ResourceDetails.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import resourceDetailsStyles from '@/styles/routes/tabs/home/ResourceDetailsScreen';
+
+interface ResourceDetailsProps {
+    types: string[];
+    topics: string[];
+    levels: string[];
+}
+
+const ResourceDetails: React.FC<ResourceDetailsProps> = ({types, topics, levels}) => {
+    return (
+        <>
+            <View style={resourceDetailsStyles.details}>
+                <Text style={resourceDetailsStyles.detailText}>
+                    <Text style={resourceDetailsStyles.label}>Types: </Text>
+                    {types.join(', ')}
+                </Text>
+                <Text style={resourceDetailsStyles.detailText}>
+                    <Text style={resourceDetailsStyles.label}>Topics: </Text>
+                    {topics.join(', ')}
+                </Text>
+            </View>
+
+            <View style={resourceDetailsStyles.levels}>
+                <Text style={resourceDetailsStyles.detailText}>
+                    <Text style={resourceDetailsStyles.label}>Levels: </Text>
+                    {levels.join(', ')}
+                </Text>
+            </View>
+        </>
+    );
+};
+
+export default ResourceDetails;
