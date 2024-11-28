c233126d5f7bdc61ddbd4447c4ce70ffa1ff40c5
DenGian <DenGian@users.noreply.github.com>
2024-11-29 00:08:22 +0100

    Implemented ResourceDetailContent component

--- screens/details.tsx
+++ screens/details.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import {View, Text, ScrollView} from 'react-native';
+import {View} from 'react-native';
 import LoadingSpinner from "@/components/ui/LoadingSpinner";
 import ErrorMessage from '@/components/ui/ErrorMessage';
-import BackButton from '@/components/ui/BackButton';
 import resourceDetailsStyles from '@/styles/routes/tabs/home/ResourceDetailsScreen';
 import useDetailResource from "@/hooks/api/useDetailResource";
-import ResourceHeaderSection from "@/components/route/tabs/home/ResourceHeaderSection";
-import ResourceDetailsSection from "@/components/route/tabs/home/ResourceDetailsSection";
-import ResourceMapSection from "@/components/route/tabs/home/ResourseMapSection";
-import VisitSourceButton from '@/components/route/tabs/home/VisitSourceButton';
+import ResourceDetailContent from '@/components/route/tabs/home/ResourceDetailContent';
 
 interface ResourceDetailsScreenProps {
     id: string;
@@ -21,25 +17,11 @@ const ResourceDetailsScreen: React.FC<ResourceDetailsScreenProps> = ({id}) => {
     if (error) return <ErrorMessage message={`Error loading resources: ${error}`}/>;
     if (!resource) return <ErrorMessage message="Resource not found"/>;
 
-    const hasEventMetadata = resource.metaData && resource.metaData.date && resource.metaData.location;
+    const hasEventMetadata = !!(resource.metaData && resource.metaData.date && resource.metaData.location);
 
     return (
         <View style={resourceDetailsStyles.container}>
-            <ScrollView contentContainerStyle={resourceDetailsStyles.centeredContent}>
-                <View style={resourceDetailsStyles.card}>
-                    <ResourceHeaderSection resource={resource}/>
-
-                    <ResourceDetailsSection resource={resource}/>
-
-                    {hasEventMetadata && resource.metaData && (
-                        <ResourceMapSection metaData={resource.metaData}/>
-                    )}
-
-                    <VisitSourceButton url={resource.url}/>
-
-                </View>
-                <BackButton/>
-            </ScrollView>
+            <ResourceDetailContent resource={resource} hasEventMetadata={hasEventMetadata}/>
         </View>
     );
 };
