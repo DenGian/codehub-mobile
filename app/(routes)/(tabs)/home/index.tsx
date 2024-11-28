914aa7851cf3c3ea2fe3a2d615af4c4a8d91820b
DenGian <DenGian@users.noreply.github.com>
2024-11-28 01:35:52 +0100

    Tracking favorites in state

--- app/(routes)/(tabs)/home/index.tsx
+++ app/(routes)/(tabs)/home/index.tsx
@@ -1,5 +1,4 @@
-// screens/HomeScreen.tsx
-import React from 'react';
+import React, {useState} from 'react';
 import {FlatList, View, Text} from 'react-native';
 import EventCard from "@/components/route/tabs/home/eventCard";
 import {useCodingResources} from '@/hooks/api/useCodingResources';
@@ -9,18 +8,28 @@ import {ListRenderItemInfo} from 'react-native';
 
 const HomeScreen: React.FC = () => {
     const {resources, loading, error} = useCodingResources();
+    const [favorites, setFavorites] = useState<number[]>([]);
+    
+    const toggleFavorite = (id: number) => {
+        setFavorites((prevFavorites) =>
+            prevFavorites.includes(id)
+                ? prevFavorites.filter((favId) => favId !== id)
+                : [...prevFavorites, id]
+        );
+    };
 
     const renderItem = ({item}: ListRenderItemInfo<CodingResource>) => {
+        const isFavorite = favorites.includes(item.id);
         return (
             <EventCard
                 id={item.id}
                 title={item.description}
                 types={item.types.join(', ')}
                 topics={item.topics.join(', ')}
-                isFavorite={false}
-                onToggleFavorite={() => {
-                }}
+                isFavorite={isFavorite}
+                onToggleFavorite={() => toggleFavorite(item.id)}
                 onDetailsPress={() => {
+                    // Placeholder for details press functionality
                 }}
             />
         );
