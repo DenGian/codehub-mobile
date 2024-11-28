90a5047ff7a970af406b9e7c2d790a4a24ebb027
DenGian <DenGian@users.noreply.github.com>
2024-11-28 20:36:05 +0100

    Rename

--- styles/routes/tabs/home/ResourceDetailsScreen.ts
+++ styles/routes/tabs/home/ResourceDetailsScreen.ts
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
 
-const styles = StyleSheet.create({
+const resourceDetailsStyles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#f9f9f9',
@@ -78,4 +78,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default styles;
+export default resourceDetailsStyles;
