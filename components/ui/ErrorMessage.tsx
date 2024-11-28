51087184bf8cc5add092aee1686a19c758e3ef81
DenGian <DenGian@users.noreply.github.com>
2024-11-28 22:56:49 +0100

    Made ErrorMessage component

---
+++ components/ui/ErrorMessage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import resourceDetailsStyles from '@/styles/routes/tabs/home/ResourceDetailsScreen';
+
+interface ErrorMessageProps {
+    message: string;
+}
+
+const ErrorMessage: React.FC<ErrorMessageProps> = ({message}) => {
+    return (
+        <View style={resourceDetailsStyles.errorContainer}>
+            <Text style={resourceDetailsStyles.error}>{message}</Text>
+        </View>
+    );
+};
+
+export default ErrorMessage;
