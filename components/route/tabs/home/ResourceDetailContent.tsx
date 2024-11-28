179170b9dffc33d5770d5d8992fe6245f4323b1a
DenGian <DenGian@users.noreply.github.com>
2024-11-29 00:08:00 +0100

    Add ResourceDetailContent component

---
+++ components/route/tabs/home/ResourceDetailContent.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, ScrollView} from 'react-native';
+import ResourceHeaderSection from '@/components/route/tabs/home/ResourceHeaderSection';
+import ResourceDetailsSection from '@/components/route/tabs/home/ResourceDetailsSection';
+import ResourceMapSection from "@/components/route/tabs/home/ResourseMapSection";
+import BackButton from "@/components/ui/BackButton";
+import VisitSourceButton from '@/components/route/tabs/home/VisitSourceButton';
+import resourceDetailsStyles from '@/styles/routes/tabs/home/ResourceDetailsScreen';
+import {CodingResource} from '@/services/api/types';
+
+interface ResourceDetailContentProps {
+    resource: CodingResource;
+    hasEventMetadata: boolean;
+}
+
+const ResourceDetailContent: React.FC<ResourceDetailContentProps> = ({resource, hasEventMetadata}) => {
+    return (
+        <ScrollView contentContainerStyle={resourceDetailsStyles.centeredContent}>
+            <View style={resourceDetailsStyles.card}>
+                <ResourceHeaderSection resource={resource}/>
+                <ResourceDetailsSection resource={resource}/>
+                {hasEventMetadata && resource.metaData && (
+                    <ResourceMapSection metaData={resource.metaData}/>
+                )}
+                <VisitSourceButton url={resource.url}/>
+            </View>
+            <BackButton/>
+        </ScrollView>
+    );
+};
+
+export default ResourceDetailContent;
