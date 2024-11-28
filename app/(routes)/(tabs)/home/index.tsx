9a683ca6113fe9476cf3289d16c313efb4782dcc
DenGian <DenGian@users.noreply.github.com>
2024-11-28 01:42:35 +0100

    Implemented hook for favorites

--- app/(routes)/(tabs)/home/index.tsx
+++ app/(routes)/(tabs)/home/index.tsx
@@ -1,22 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {FlatList, View, Text} from 'react-native';
 import EventCard from "@/components/route/tabs/home/eventCard";
 import {useCodingResources} from '@/hooks/api/useCodingResources';
 import LoadingSpinner from '@/components/ui/LoadingSpinner';
 import {CodingResource} from "@/services/api/types";
 import {ListRenderItemInfo} from 'react-native';
+import {useFavorites} from "@/hooks/storage/useFavorites";
 
 const HomeScreen: React.FC = () => {
     const {resources, loading, error} = useCodingResources();
-    const [favorites, setFavorites] = useState<number[]>([]);
-    
-    const toggleFavorite = (id: number) => {
-        setFavorites((prevFavorites) =>
-            prevFavorites.includes(id)
-                ? prevFavorites.filter((favId) => favId !== id)
-                : [...prevFavorites, id]
-        );
-    };
+    const {favorites, toggleFavorite} = useFavorites();
 
     const renderItem = ({item}: ListRenderItemInfo<CodingResource>) => {
         const isFavorite = favorites.includes(item.id);
