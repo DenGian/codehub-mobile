b2c0868fc89e1c80b6877c42adcda61c34c1cc02
DenGian <DenGian@users.noreply.github.com>
2024-11-28 01:19:46 +0100

    WIP

--- app/(routes)/(tabs)/home/index.tsx
+++ app/(routes)/(tabs)/home/index.tsx
@@ -1,41 +1,26 @@
+// screens/HomeScreen.tsx
 import React from 'react';
-import {FlatList, View, Text} from 'react-native';
-import {useCodingResources} from "@/hooks/api/useCodingResources";
-import {CodingResource} from "@/services/api/types"; // Import the type for type checking
+import {FlatList, View, Text, ScrollView} from 'react-native';
+import EventCard from "@/components/route/tabs/home/eventCard";
+import {useCodingResources} from '@/hooks/api/useCodingResources';
+import LoadingSpinner from '@/components/ui/LoadingSpinner';
 
 const HomeScreen: React.FC = () => {
     const {resources, loading, error} = useCodingResources();
 
-    // Handle loading state
-    if (loading) {
-        return <Text>Loading...</Text>;
-    }
-
-    // Handle error state
-    if (error) {
-        return <Text>Error: {error}</Text>;
-    }
-
-    // Render individual coding resource items
-    const renderItem = ({item}: { item: CodingResource }) => (
-        <View style={{marginBottom: 10, padding: 10, borderWidth: 1, borderColor: 'gray'}}>
-            <Text style={{fontSize: 18, fontWeight: 'bold'}}>{item.description}</Text>
-            <Text>Types: {item.types.join(', ')}</Text>
-            <Text>Topics: {item.topics.join(', ')}</Text>
-            <Text>Levels: {item.levels.join(', ')}</Text>
-            <Text style={{color: 'blue'}}>{item.url}</Text>
-        </View>
-    );
+    if (loading) return <LoadingSpinner visible={true}/>;
+    if (error) return <Text>Error: {error}</Text>;
 
     return (
-        <View style={{flex: 1, padding: 16}}>
-            <Text style={{fontSize: 24, fontWeight: 'bold', marginBottom: 20}}>Coding Resources</Text>
-            <FlatList
-                data={resources}  // Use the fetched resources array
-                renderItem={renderItem}  // Render each resource as a simple view
-                keyExtractor={(item) => item.id.toString()}  // Unique key for each item
-            />
-        </View>
+        <ScrollView style={{flex: 1}}>
+            <View style={{padding: 20}}>
+                <FlatList
+                    data={resources}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => <EventCard event={item}/>}
+                />
+            </View>
+        </ScrollView>
     );
 };
 
