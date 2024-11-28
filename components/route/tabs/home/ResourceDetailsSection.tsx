87df76e25ccb5ecea7b57d0f235b97a89d1a44cc
DenGian <DenGian@users.noreply.github.com>
2024-11-28 23:48:32 +0100

    Add ResourceDetailsSection component

---
+++ components/route/tabs/home/ResourceDetailsSection.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View} from 'react-native';
+import ResourceDetails from '@/components/route/tabs/home/ResourceDetails';
+import {CodingResource} from '@/services/api/types';
+
+interface ResourceDetailsSectionProps {
+    resource: CodingResource;
+}
+
+const ResourceDetailsSection: React.FC<ResourceDetailsSectionProps> = ({resource}) => {
+    return (
+        <View>
+            <ResourceDetails
+                types={resource.types}
+                topics={resource.topics}
+                levels={resource.levels}
+            />
+        </View>
+    );
+};
+
+export default ResourceDetailsSection;
