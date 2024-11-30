a0596dec8e82cea57ade4b9fa22cead2c7da04c3
DenGian <DenGian@users.noreply.github.com>
2024-12-01 00:46:26 +0100

    Made component for rendering items

---
+++ components/route/tabs/home/HomeScreenRenderItem.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {CodingResource} from '@/services/api/types';
+import EventCard from '@/components/route/tabs/home/eventCard';
+import {useRouter} from 'expo-router';
+import {useFavorites} from '@/hooks/storage/useFavorites';
+
+interface RenderItemProps {
+    item: CodingResource;
+}
+
+const HomeScreenRenderItem: React.FC<RenderItemProps> = ({item}) => {
+    const router = useRouter();
+    const {favorites, toggleFavorite} = useFavorites();
+    const isFavorite = favorites.includes(item.id);
+
+    return (
+        <EventCard
+            id={item.id}
+            title={item.description}
+            types={item.types.join(', ')}
+            topics={item.topics.join(', ')}
+            isFavorite={isFavorite}
+            onToggleFavorite={() => toggleFavorite(item.id)}
+            onDetailsPress={() => router.push(`/home/details/${item.id}`)}
+        />
+    );
+};
+
+export default HomeScreenRenderItem;
