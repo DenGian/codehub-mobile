085fb2260ddb375fb5e9a02261ba170316478dfc
DenGian <DenGian@users.noreply.github.com>
2024-11-30 14:43:10 +0100

    Add logic to filter on favourites

--- hooks/api/useFilterResources.ts
+++ hooks/api/useFilterResources.ts
@@ -1,10 +1,13 @@
 import {useState, useEffect} from 'react';
 import {useCodingResources} from '@/hooks/api/useCodingResources';
 import {CodingResource} from '@/services/api/types';
+import {useFavorites} from '@/hooks/storage/useFavorites';
 
 export const useFilterResources = () => {
     const {resources, loading, error} = useCodingResources();
+    const {favorites} = useFavorites();
     const [search, setSearch] = useState<string>('');
+    const [showFavorites, setShowFavorites] = useState<boolean>(false);
     const [filteredResources, setFilteredResources] = useState<CodingResource[]>([]);
 
     useEffect(() => {
@@ -17,9 +20,13 @@ export const useFilterResources = () => {
                 );
             }
 
+            if (showFavorites) {
+                filtered = filtered.filter(resource => favorites.includes(resource.id));
+            }
+
             setFilteredResources(filtered);
         }
-    }, [resources, search]);
+    }, [resources, search, showFavorites, favorites]);
 
     return {
         filteredResources,
@@ -27,5 +34,7 @@ export const useFilterResources = () => {
         error,
         search,
         setSearch,
+        showFavorites,
+        setShowFavorites,
     };
 };
