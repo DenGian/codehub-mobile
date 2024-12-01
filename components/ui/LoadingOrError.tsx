09687b0beed0f2a44a604718f23b547f3e6ad9ca
DenGian <DenGian@users.noreply.github.com>
2024-12-01 15:37:39 +0100

    Made LoadingOrError component

---
+++ components/ui/LoadingOrError.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import LoadingSpinner from '@/components/ui/LoadingSpinner';
+import loadingOrErrorStyles from "@/styles/ui/loadingOrErrorStyles";
+
+interface LoadingOrErrorProps {
+    loading: boolean;
+    refreshing: boolean;
+    error: string | null;
+}
+
+const LoadingOrError: React.FC<LoadingOrErrorProps> = ({loading, refreshing, error}) => {
+    if (loading && !refreshing) {
+        return <LoadingSpinner visible/>;
+    }
+
+    if (error) {
+        return (
+            <View style={loadingOrErrorStyles.errorContainer}>
+                <Text style={loadingOrErrorStyles.errorText}>{error}</Text>
+            </View>
+        );
+    }
+
+    return null;
+};
+
+export default LoadingOrError;
