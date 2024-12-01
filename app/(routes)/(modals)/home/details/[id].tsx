deedeeb4c66e467a7be629dde1ce0a31922545a9
DenGian <DenGian@users.noreply.github.com>
2024-12-01 21:22:48 +0100

    Updated import

--- app/(routes)/(modals)/home/details/[id].tsx
+++ app/(routes)/(modals)/home/details/[id].tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View} from 'react-native';
 import {useLocalSearchParams} from 'expo-router';
-import ResourceDetailsScreen from "@/screens/route/tabs/home/details";
+import ResourceDetailsScreen from "@/screens/route/tabs/home/DetailsPage";
 
 const ResourceDetailsPage: React.FC = () => {
     const {id} = useLocalSearchParams<{ id: string }>();
