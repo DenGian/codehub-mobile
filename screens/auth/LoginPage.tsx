40d88d372da319579fc6e6a9fced781fc6682491
DenGian <DenGian@users.noreply.github.com>
2024-12-01 21:53:31 +0100

    Made screen for login page

---
+++ screens/auth/LoginPage.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View} from 'react-native';
+import loginStyles from '@/styles/auth/loginStyles';
+import useLoginForm from '@/hooks/auth/useLoginForm';
+import LoginContent from '@/components/auth/LoginContent';
+
+const login = () => {
+    const {
+        emailAddress,
+        setEmailAddress,
+        password,
+        setPassword,
+        loading,
+        onSignInPress
+    } = useLoginForm();
+
+    return (
+        <View style={loginStyles.container}>
+            <LoginContent
+                loading={loading}
+                emailAddress={emailAddress}
+                setEmailAddress={setEmailAddress}
+                password={password}
+                setPassword={setPassword}
+                onSignInPress={onSignInPress}
+            />
+        </View>
+    );
+};
+
+export default login;
