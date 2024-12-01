adc1dc4bb18b947418ad0f9402b2b4bf74e363d8
DenGian <DenGian@users.noreply.github.com>
2024-12-01 21:51:57 +0100

    Bug fixing

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import AuthPages from "@/screens/auth/AuthPages";
+import {View} from 'react-native';
+import AuthPages from '@/screens/auth/AuthPages';
 
 const AuthLayout = () => {
-    return <AuthPages/>;
+    return (
+        <View style={{flex: 1}}>
+            <AuthPages/>
+        </View>
+    );
 };
 
 export default AuthLayout;
