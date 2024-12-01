0a4201125be02c4b2d214f350c9482af3e23f0db
DenGian <DenGian@users.noreply.github.com>
2024-12-01 21:52:58 +0100

    Importing screen

--- app/(auth)/reset.tsx
+++ app/(auth)/reset.tsx
@@ -1,41 +1,13 @@
-import {View} from 'react-native';
 import React from 'react';
-import resetStyles from '@/styles/auth/resetStyles';
-import usePasswordReset from '@/hooks/auth/usePasswordReset';
-import HeaderBack from '@/components/ui/HeaderBack';
-import ResetContent from '@/components/auth/ResetContent';
+import {View} from 'react-native';
+import ResetScreen from '@/screens/auth/ResetPage';
 
-const PwReset = () => {
-    const {
-        emailAddress,
-        setEmailAddress,
-        password,
-        setPassword,
-        code,
-        setCode,
-        successfulCreation,
-        onRequestReset,
-        onReset,
-        loading,
-    } = usePasswordReset();
-
+const ResetPage: React.FC = () => {
     return (
-        <View style={resetStyles.container}>
-            <HeaderBack visible={!successfulCreation}/>
-            <ResetContent
-                emailAddress={emailAddress}
-                setEmailAddress={setEmailAddress}
-                code={code}
-                setCode={setCode}
-                password={password}
-                setPassword={setPassword}
-                onRequestReset={onRequestReset}
-                onReset={onReset}
-                successfulCreation={successfulCreation}
-                loading={loading}
-            />
+        <View style={{flex: 1}}>
+            <ResetScreen/>
         </View>
     );
 };
 
-export default PwReset;
+export default ResetPage;
