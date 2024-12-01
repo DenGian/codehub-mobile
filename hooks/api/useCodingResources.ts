ae52f7d6295d23ea0da599469035cd9c1f8f3cb4
DenGian <DenGian@users.noreply.github.com>
2024-12-01 01:56:10 +0100

    Add Fletlist refreshing

--- hooks/api/useCodingResources.ts
+++ hooks/api/useCodingResources.ts
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useCallback} from 'react';
 import {fetchCodingResources} from "@/services/api/codingResources";
 import {CodingResource} from "@/services/api/types";
 
@@ -7,23 +7,30 @@ export const useCodingResources = () => {
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        const getResources = async () => {
-            try {
-                const data = await fetchCodingResources();
-                console.log('Fetched coding resources:', data);
-                setResources(data);
-            } catch (err: any) {
-                console.error('Error fetching coding resources:', err.message || 'Failed to fetch data');
-                setError(err.message || 'Failed to fetch data');
-            } finally {
-                setLoading(false);
-            }
-        };
+    const getResources = useCallback(async () => {
+        try {
+            setLoading(true);
+            const data = await fetchCodingResources();
+            console.log('Fetched coding resources:', data);
+            setResources(data);
+            setError(null);
+        } catch (err: any) {
+            console.error('Error fetching coding resources:', err.message || 'Failed to fetch data');
+            setError(err.message || 'Failed to fetch data');
+        } finally {
+            setLoading(false);
+        }
+    }, []);
 
+    useEffect(() => {
         getResources().then(() => {
         });
     }, []);
 
-    return {resources, loading, error};
+    return {
+        resources,
+        loading,
+        error,
+        reFetch: getResources
+    };
 };
