cb22cc89600186be72709a41e969dda5338b7cac
DenGian <DenGian@users.noreply.github.com>
2024-12-01 21:53:52 +0100

    Made screen for reset page

---
+++ screens/auth/ResetPage.tsx
@@ -0,0 +1,41 @@
+import {View} from 'react-native';
+import React from 'react';
+import resetStyles from '@/styles/auth/resetStyles';
+import usePasswordReset from '@/hooks/auth/usePasswordReset';
+import HeaderBack from '@/components/ui/HeaderBack';
+import ResetContent from '@/components/auth/ResetContent';
+
+const PwReset = () => {
+    const {
+        emailAddress,
+        setEmailAddress,
+        password,
+        setPassword,
+        code,
+        setCode,
+        successfulCreation,
+        onRequestReset,
+        onReset,
+        loading,
+    } = usePasswordReset();
+
+    return (
+        <View style={resetStyles.container}>
+            <HeaderBack visible={!successfulCreation}/>
+            <ResetContent
+                emailAddress={emailAddress}
+                setEmailAddress={setEmailAddress}
+                code={code}
+                setCode={setCode}
+                password={password}
+                setPassword={setPassword}
+                onRequestReset={onRequestReset}
+                onReset={onReset}
+                successfulCreation={successfulCreation}
+                loading={loading}
+            />
+        </View>
+    );
+};
+
+export default PwReset;
