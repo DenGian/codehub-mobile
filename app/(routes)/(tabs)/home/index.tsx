4d7e390fc2075f1d306f6b4f9bf39db3902981c2
DenGian <DenGian@users.noreply.github.com>
2024-12-01 21:25:54 +0100

    Imported HomeScreen

--- app/(routes)/(tabs)/home/index.tsx
+++ app/(routes)/(tabs)/home/index.tsx
@@ -1,45 +1,13 @@
 import React from 'react';
 import {View} from 'react-native';
-import {useFilterResources} from '@/hooks/api/useFilterResources';
-import FilterBar from '@/components/ui/FilterBar';
-import FilteredList from '@/components/route/tabs/home/FilteredList';
-import LoadingOrError from '@/components/ui/LoadingOrError';
-import homeScreenStyles from "@/styles/routes/tabs/home/homeScreenStyles";
-import {useLazyLoading} from "@/hooks/route/tabs/home/useLazyLoading";
+import HomeScreen from "@/screens/route/tabs/home/HomePage";
 
-const HomeScreen: React.FC = () => {
-    const {
-        filteredResources,
-        loading,
-        error,
-        search,
-        setSearch,
-        showFavorites,
-        setShowFavorites,
-        refreshing,
-        handleRefresh
-    } = useFilterResources();
-
-    const {visibleItems, setVisibleItems} = useLazyLoading(filteredResources);
-
+const HomePage: React.FC = () => {
     return (
-        <View style={homeScreenStyles.container}>
-            <LoadingOrError loading={loading} refreshing={refreshing} error={error}/>
-            <FilterBar
-                searchValue={search}
-                onSearchChange={setSearch}
-                showFavorites={showFavorites}
-                onShowFavoritesChange={setShowFavorites}
-            />
-            <FilteredList
-                visibleItems={visibleItems}
-                setVisibleItems={setVisibleItems}
-                filteredResources={filteredResources}
-                refreshing={refreshing}
-                handleRefresh={handleRefresh}
-            />
+        <View style={{flex: 1}}>
+            <HomeScreen/>
         </View>
     );
 };
 
-export default HomeScreen;
+export default HomePage;
