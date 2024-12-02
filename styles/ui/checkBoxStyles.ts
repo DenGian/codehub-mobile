1ac1872d9d9d1a7b0eca8b153385326aa5246ab9
DenGian <DenGian@users.noreply.github.com>
2024-12-02 20:04:30 +0100

    Add styles for CheckBox component

---
+++ styles/ui/checkBoxStyles.ts
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+const checkBoxStyles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    label: {
+        marginRight: 10,
+        fontSize: 16,
+    },
+});
+
+export default checkBoxStyles;
