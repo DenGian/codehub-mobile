ce7654fa9284332834f80ccb2f5a0d4644f3f199
DenGian <DenGian@users.noreply.github.com>
2024-12-02 22:18:10 +0100

    Fixed bug were components were not rendered

--- styles/routes/tabs/profile/profileStyles.ts
+++ styles/routes/tabs/profile/profileStyles.ts
@@ -2,7 +2,7 @@ import {StyleSheet} from 'react-native';
 
 const profileStyles = StyleSheet.create({
     container: {
-        flex: 1,
+        flexGrow: 1,
         justifyContent: 'center',
         padding: 40,
     },
