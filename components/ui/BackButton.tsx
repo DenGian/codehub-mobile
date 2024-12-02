4ff7f9d3c2348c6c465f21adbc60b71ec121edf6
DenGian <DenGian@users.noreply.github.com>
2024-12-02 19:53:34 +0100

    Imported styles

--- components/ui/BackButton.tsx
+++ components/ui/BackButton.tsx
@@ -1,31 +1,16 @@
 import React from 'react';
-import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+import {TouchableOpacity, Text} from 'react-native';
 import {useRouter} from 'expo-router';
+import backButtonStyles from '@/styles/ui/backButtonStyles';
 
 const BackButton: React.FC = () => {
     const router = useRouter();
 
     return (
-        <TouchableOpacity style={styles.button} onPress={() => router.back()}>
-            <Text style={styles.buttonText}>Back</Text>
+        <TouchableOpacity style={backButtonStyles.button} onPress={() => router.back()}>
+            <Text style={backButtonStyles.buttonText}>Back</Text>
         </TouchableOpacity>
     );
 };
-
-const styles = StyleSheet.create({
-    button: {
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        backgroundColor: '#007bff',
-        borderRadius: 5,
-        alignItems: 'center',
-        margin: 20,
-    },
-    buttonText: {
-        color: '#fff',
-        fontSize: 16,
-        fontWeight: '600',
-    },
-});
 
 export default BackButton;
