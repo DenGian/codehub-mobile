bef017eb0a39f211744bfc1957ec98b1b8be5f1c
DenGian <DenGian@users.noreply.github.com>
2024-12-02 20:07:02 +0100

    Add styles for GreetingText component

---
+++ styles/ui/greetingTextStyles.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+const greetingTextStyles = StyleSheet.create({
+    greetingText: {
+        textAlign: 'center',
+    },
+});
+
+export default greetingTextStyles;
