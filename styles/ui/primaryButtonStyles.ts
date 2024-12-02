8159f9da1f8d114f6eca43a61d4879493a5dc956
DenGian <DenGian@users.noreply.github.com>
2024-12-02 20:10:09 +0100

    Add styles for PrimaryButton component

---
+++ styles/ui/primaryButtonStyles.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+const primaryButtonStyles = StyleSheet.create({
+    buttonContainer: {
+        marginVertical: 8,
+    },
+});
+
+export default primaryButtonStyles;
