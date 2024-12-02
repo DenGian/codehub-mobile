d9b21bdee4f0c7a9143d88ab8d300df5ff52dab2
DenGian <DenGian@users.noreply.github.com>
2024-12-02 19:50:13 +0100

    Made styles for VisitSourceButton component

---
+++ styles/routes/tabs/home/visitSourceButtonStyles.ts
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const visitSourceButtonStyles = StyleSheet.create({
+    button: {
+        marginTop: 10,
+        paddingVertical: 8,
+        paddingHorizontal: 15,
+        backgroundColor: '#007bff',
+        borderRadius: 5,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '600',
+    },
+});
+
+export default visitSourceButtonStyles;
