43a50c4fd168648ee6ee8df37736114d02cf5e64
DenGian <DenGian@users.noreply.github.com>
2024-12-02 20:08:48 +0100

    Imported styles

--- components/ui/InputField.tsx
+++ components/ui/InputField.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {TextInput, StyleSheet} from 'react-native';
+import {TextInput} from 'react-native';
+import inputFieldStyles from '@/styles/ui/inputFieldStyles';
 
 interface InputFieldProps {
     placeholder: string;
@@ -14,21 +15,9 @@ const InputField: React.FC<InputFieldProps> = ({placeholder, value, onChangeText
             placeholderTextColor="black"
             value={value}
             onChangeText={onChangeText}
-            style={styles.inputField}
+            style={inputFieldStyles.inputField}
         />
     );
 };
-
-const styles = StyleSheet.create({
-    inputField: {
-        marginVertical: 4,
-        height: 50,
-        borderWidth: 1,
-        borderColor: '#6c47ff',
-        borderRadius: 4,
-        padding: 10,
-        backgroundColor: '#fff',
-    },
-});
 
 export default InputField;
