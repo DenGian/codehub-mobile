b28b074adb0e411cf4a75af6cf59e29575f1679b
DenGian <DenGian@users.noreply.github.com>
2024-12-02 20:08:36 +0100

    Add styles for InputField component

---
+++ styles/ui/inputFieldStyles.ts
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+const inputFieldStyles = StyleSheet.create({
+    inputField: {
+        marginVertical: 4,
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#6c47ff',
+        borderRadius: 4,
+        padding: 10,
+        backgroundColor: '#fff',
+    },
+});
+
+export default inputFieldStyles;
