fdf038d5a3a61314146a24fd7f23d481b6486e27
DenGian <DenGian@users.noreply.github.com>
2024-12-02 20:11:54 +0100

    Add styles for SearchBar component

---
+++ styles/ui/searchBarStyles.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const searchBarStyles = StyleSheet.create({
+    container: {
+        marginBottom: 10,
+    },
+    input: {
+        height: 40,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingHorizontal: 10,
+    },
+});
+
+export default searchBarStyles;
