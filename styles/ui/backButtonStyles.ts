9b28fa7a70ebebc484dae69aca3c7bb88511907b
DenGian <DenGian@users.noreply.github.com>
2024-12-02 19:53:18 +0100

    Add styles for BackButton component

---
+++ styles/ui/backButtonStyles.ts
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const backButtonStyles = StyleSheet.create({
+    button: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#007bff',
+        borderRadius: 5,
+        alignItems: 'center',
+        margin: 20,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '600',
+    },
+});
+
+export default backButtonStyles;
