2c871bebb70d2df1f520115e01aab5f5e26d4498
DenGian <DenGian@users.noreply.github.com>
2024-12-04 17:37:29 +0100

    Imported ProfilePicture component

--- components/route/tabs/profile/ProfileContent.tsx
+++ components/route/tabs/profile/ProfileContent.tsx
@@ -4,6 +4,7 @@ import GreetingText from '@/components/route/tabs/profile/GreetingText';
 import UserInputFields from '@/components/ui/UserInputFields';
 import PrimaryButton from '@/components/ui/PrimaryButton';
 import profileStyles from '@/styles/routes/tabs/profile/profileStyles';
+import ProfilePicture from "@/components/route/tabs/profile/ProfilePicture";
 
 interface ProfileContentProps {
     firstName: string;
@@ -18,6 +19,7 @@ const ProfileContent: React.FC<ProfileContentProps> = (
     {firstName, setFirstName, lastName, setLastName, loading, onSaveUser}) => {
     return (
         <View style={profileStyles.container}>
+            <ProfilePicture/>
             <GreetingText firstName={firstName} lastName={lastName}/>
             <UserInputFields
                 firstName={firstName}
