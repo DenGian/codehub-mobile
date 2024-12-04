5b49545f2b785d7770a666ca627f6aa5768c09ac
DenGian <DenGian@users.noreply.github.com>
2024-12-04 20:12:03 +0100

    Add ProfilePictureProvider

--- app/(routes)/(tabs)/_layout.tsx
+++ app/(routes)/(tabs)/_layout.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import TabsNavigator from "@/navigation/TabsNavigator";
+import {ProfilePictureProvider} from "@/context/ProfilePictureContext";
 
 const TabsLayout = () => {
-    return <TabsNavigator/>;
+    return (
+        <ProfilePictureProvider>
+            <TabsNavigator/>
+        </ProfilePictureProvider>
+    )
 };
 
 export default TabsLayout;
