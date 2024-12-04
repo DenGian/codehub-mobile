899110491ee67304c99956b742c0c42086d3dc9d
DenGian <DenGian@users.noreply.github.com>
2024-12-04 20:42:02 +0100

    Imported profilePictureStyles

---
+++ styles/routes/tabs/profile/profilePictureStyles.ts
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+const profilePictureStyles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    profilePicture: {
+        width: 120,
+        height: 120,
+        borderRadius: 60,
+        borderWidth: 1,
+        borderColor: '#6c47ff',
+    },
+});
+
+export default profilePictureStyles;
