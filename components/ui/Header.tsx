9f5b4e586b75979b00fa9273c4e2980397dd5063
DenGian <DenGian@users.noreply.github.com>
2024-12-04 20:15:30 +0100

    Implemented useProfilePicture

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import {useUser} from '@clerk/clerk-expo';
+import {useProfilePicture} from '@/context/ProfilePictureContext';
 import customHeaderStyles from '@/styles/ui/customHeaderStyles';
 
 const CustomHeader: React.FC = () => {
     const {user} = useUser();
     const displayName = user?.firstName || user?.username;
+    const {profilePicture} = useProfilePicture();
 
     return (
         <View style={customHeaderStyles.headerContainer}>
-            <Text style={customHeaderStyles.headerText}>Hey, {displayName}!</Text>
+            {profilePicture ? (
+                <Image source={{uri: profilePicture}} style={styles.profilePicture}/>
+            ) : (
+                <Text style={customHeaderStyles.headerText}>Hey, {displayName}!</Text>
+            )}
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    profilePicture: {
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+    },
+});
 
 export default CustomHeader;
