06aed9cedf4ed89f366427d98a43ec776baf04ce
DenGian <DenGian@users.noreply.github.com>
2024-12-08 23:07:09 +0100

    Rm headers

--- services/api/codingResources.ts
+++ services/api/codingResources.ts
@@ -2,21 +2,9 @@ import {apiClient} from "./client";
 import {CodingResource} from "./types";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const API_TOKEN = process.env.EXPO_PUBLIC_API_TOKEN;
-
-if (!API_TOKEN) {
-    throw new Error("API token is missing in .env!");
-}
-
 export const fetchCodingResources = async (): Promise<CodingResource[]> => {
     try {
-        const apiResources = await apiClient<CodingResource[]>("/codingResources", {
-            headers: {
-                "Authorization": `Bearer ${API_TOKEN}`,
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-            },
-        });
+        const apiResources = await apiClient<CodingResource[]>("/codingResources");
 
         const storedFavorites = await AsyncStorage.getItem("favorites");
         const favorites: number[] = storedFavorites ? JSON.parse(storedFavorites) : [];
