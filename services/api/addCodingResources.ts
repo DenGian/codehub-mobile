73cb481ef3f4a6a2239c6f5f67d7dbdb1ab6ebd9
DenGian <DenGian@users.noreply.github.com>
2024-12-08 23:06:08 +0100

    Rm headers

--- services/api/addCodingResources.ts
+++ services/api/addCodingResources.ts
@@ -1,12 +1,6 @@
 import {CodingResource} from "@/services/api/types";
 import {apiClient} from "@/services/api/client";
 
-const API_TOKEN = process.env.EXPO_PUBLIC_API_TOKEN;
-
-if (!API_TOKEN) {
-    throw new Error("API token is missing in .env!");
-}
-
 export const addCodingResources = async (codingResource: CodingResource): Promise<CodingResource> => {
     if (!codingResource) {
         throw new Error("Invalid coding resource data.");
@@ -14,11 +8,6 @@ export const addCodingResources = async (codingResource: CodingResource): Promis
     try {
         const apiResponse = await apiClient<CodingResource>("/codingResources", {
             method: "POST",
-            headers: {
-                "Authorization": `Bearer ${API_TOKEN}`,
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-            },
             body: JSON.stringify(codingResource),
         });
         console.log("Resource added successfully:", apiResponse);
