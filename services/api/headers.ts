a67af0eccedf924add9f602bd10ac3badf8f3551
DenGian <DenGian@users.noreply.github.com>
2024-12-08 23:07:22 +0100

    Centralized headers

---
+++ services/api/headers.ts
@@ -0,0 +1,11 @@
+const API_TOKEN = process.env.EXPO_PUBLIC_API_TOKEN;
+
+if (!API_TOKEN) {
+    throw new Error("API token is missing in .env!");
+}
+
+export const getHeaders = () => ({
+    "Authorization": `Bearer ${API_TOKEN}`,
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+});
