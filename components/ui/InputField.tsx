43e745f8856d373c570e4318af405a03ed959e09
DenGian <DenGian@users.noreply.github.com>
2024-12-09 22:41:39 +0100

    Add keyboardType for UserExperience

--- components/ui/InputField.tsx
+++ components/ui/InputField.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import {TextInput} from 'react-native';
+import {TextInput, KeyboardTypeOptions} from 'react-native';
 import inputFieldStyles from '@/styles/ui/inputFieldStyles';
 
 interface InputFieldProps {
     placeholder: string;
     value: string;
     onChangeText: (text: string) => void;
+    keyboardType?: KeyboardTypeOptions;
 }
 
-const InputField: React.FC<InputFieldProps> = ({placeholder, value, onChangeText}) => {
+const InputField: React.FC<InputFieldProps> = ({placeholder, value, onChangeText, keyboardType}) => {
     return (
         <TextInput
             placeholder={placeholder}
@@ -16,6 +17,7 @@ const InputField: React.FC<InputFieldProps> = ({placeholder, value, onChangeText
             value={value}
             onChangeText={onChangeText}
             style={inputFieldStyles.inputField}
+            keyboardType={keyboardType}
         />
     );
 };
