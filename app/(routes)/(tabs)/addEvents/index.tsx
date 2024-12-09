2f1107fe2e8c1001ab30d792a4e1663b04f50770
DenGian <DenGian@users.noreply.github.com>
2024-12-09 22:42:04 +0100

    Implemented keyboardType for UX

--- app/(routes)/(tabs)/addEvents/index.tsx
+++ app/(routes)/(tabs)/addEvents/index.tsx
@@ -62,13 +62,15 @@ const AddEventForm: React.FC = () => {
     return (
         <View style={styles.container}>
             <InputField placeholder="Description*" value={description} onChangeText={setDescription}/>
-            <InputField placeholder="URL*" value={url} onChangeText={setUrl}/>
+            <InputField placeholder="URL*" value={url} onChangeText={setUrl} keyboardType="url"/>
             <InputField placeholder="Types* (comma-separated)" value={types} onChangeText={setTypes}/>
             <InputField placeholder="Topics* (comma-separated)" value={topics} onChangeText={setTopics}/>
             <InputField placeholder="Levels* (comma-separated)" value={levels} onChangeText={setLevels}/>
-            <InputField placeholder="Date" value={date} onChangeText={setDate}/>
-            <InputField placeholder="Latitude" value={lat} onChangeText={setLat}/>
-            <InputField placeholder="Longitude" value={long} onChangeText={setLong}/>
+            <InputField placeholder="Date" value={date} onChangeText={setDate} keyboardType="numbers-and-punctuation"/>
+            <InputField placeholder="Latitude" value={lat} onChangeText={setLat}
+                        keyboardType="numbers-and-punctuation"/>
+            <InputField placeholder="Longitude" value={long} onChangeText={setLong}
+                        keyboardType="numbers-and-punctuation"/>
             <Button title="Add Resource" onPress={handleSubmit}/>
         </View>
     );
