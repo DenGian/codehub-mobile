19f167ec7a41c41eba8d10d49d00c6f9b2dbfead
DenGian <DenGian@users.noreply.github.com>
2024-12-12 20:36:11 +0100

    Updated input fields for better UX

--- components/ui/InputField.tsx
+++ components/ui/InputField.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import {TextInput, KeyboardTypeOptions} from 'react-native';
-import inputFieldStyles from '@/styles/ui/inputFieldStyles';
+import React, {useState} from 'react';
+import {TextInput, View, Text, StyleSheet, KeyboardTypeOptions} from 'react-native';
 
 interface InputFieldProps {
     placeholder: string;
@@ -10,16 +9,45 @@ interface InputFieldProps {
 }
 
 const InputField: React.FC<InputFieldProps> = ({placeholder, value, onChangeText, keyboardType}) => {
+    const [isFocused, setIsFocused] = useState(false);
+
     return (
-        <TextInput
-            placeholder={placeholder}
-            placeholderTextColor="black"
-            value={value}
-            onChangeText={onChangeText}
-            style={inputFieldStyles.inputField}
-            keyboardType={keyboardType}
-        />
+        <View style={styles.inputContainer}>
+            {(isFocused || value) && <Text style={styles.label}>{placeholder}</Text>}
+            <TextInput
+                style={styles.inputField}
+                value={value}
+                onChangeText={onChangeText}
+                keyboardType={keyboardType}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+                placeholder={!isFocused ? placeholder : ''}
+                placeholderTextColor="grey"
+            />
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginBottom: 16,
+        position: 'relative',
+    },
+    label: {
+        position: 'absolute',
+        top: -10,
+        left: 10,
+        fontSize: 12,
+        color: 'grey',
+    },
+    inputField: {
+        borderBottomWidth: 1,
+        borderColor: '#ccc',
+        paddingVertical: 8,
+        paddingHorizontal: 10,
+        fontSize: 16,
+        color: 'black',
+    },
+});
 
 export default InputField;
