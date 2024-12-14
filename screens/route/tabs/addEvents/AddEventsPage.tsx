8a44584a86c54595f6c02910e062a35228f98bc9
DenGian <DenGian@users.noreply.github.com>
2024-12-14 14:43:52 +0100

    Made screen for Add events screen

---
+++ screens/route/tabs/addEvents/AddEventsPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AddEventsContent from "@/components/route/tabs/addEvents/AddEventsContent";
+
+const AddEventsScreen: React.FC = () => {
+    return <AddEventsContent/>;
+};
+
+export default AddEventsScreen;
