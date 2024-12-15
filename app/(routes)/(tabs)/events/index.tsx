73d2030540840f575ecb3b5087e7ae85733c9e25
DenGian <DenGian@users.noreply.github.com>
2024-12-15 17:17:34 +0100

    Imported EventsContent component

--- app/(routes)/(tabs)/events/index.tsx
+++ app/(routes)/(tabs)/events/index.tsx
@@ -1,40 +1,11 @@
 import React, {useState} from 'react';
-import {View, Text, ScrollView} from 'react-native';
-import {Calendar} from 'react-native-calendars';
-import {useCodingResources} from '@/hooks/api/useCodingResources';
-import LoadingOrError from '@/components/ui/LoadingOrError';
-import calendarStyles from '@/styles/routes/tabs/events/calendarStyles';
-import EventCard from "@/components/route/tabs/events/EventCard";
-import {markDates} from '@/utils/markDates';
-import {formatDate} from '@/utils/formatDate';
+import EventsContent from "@/components/route/tabs/events/EventsContent";
 
 const CustomCalendarScreen = () => {
     const [selectedDate, setSelectedDate] = useState<string>('');
-    const {resources: events, loading, error} = useCodingResources();
-
-    const filteredEvents = events?.filter(event => event.metaData?.date && formatDate(event.metaData.date) === selectedDate);
-
-    const markedDates = markDates(events || undefined, selectedDate);
 
     return (
-        <View style={calendarStyles.container}>
-            <Calendar
-                onDayPress={(day: { dateString: string }) => setSelectedDate(day.dateString)}
-                markedDates={markedDates}
-            />
-            <ScrollView style={calendarStyles.eventsContainer}>
-                <LoadingOrError loading={loading} refreshing={false} error={error}/>
-                {!loading && !error && (
-                    filteredEvents?.length ? (
-                        filteredEvents.map((event, index) => (
-                            <EventCard key={index} event={event}/>
-                        ))
-                    ) : (
-                        <Text style={calendarStyles.noEvents}>No events for this date</Text>
-                    )
-                )}
-            </ScrollView>
-        </View>
+        <EventsContent selectedDate={selectedDate} setSelectedDate={setSelectedDate}/>
     );
 };
 
