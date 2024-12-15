f64181cbf99a7473c27b03fe30008a50b53472ab
DenGian <DenGian@users.noreply.github.com>
2024-12-15 23:05:08 +0100

    Refactoring

--- utils/addEventsValidation.ts
+++ utils/addEventsValidation.ts
@@ -1,6 +1,6 @@
 import {Alert} from 'react-native';
 
-export const validateForm = (
+const validateForm = (
     description: string,
     url: string,
     types: string,
@@ -50,3 +50,5 @@ export const validateForm = (
 
     return true;
 };
+
+export {validateForm};
