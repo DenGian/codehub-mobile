45d7f11d6850bf7da388136043976c85ec0c5b59
DenGian <DenGian@users.noreply.github.com>
2024-12-15 23:05:18 +0100

    Refactoring

--- utils/loadMoreItems.ts
+++ utils/loadMoreItems.ts
@@ -1,7 +1,7 @@
 import {CodingResource} from '@/services/api/types';
 import {LAZY_LOAD_INCREMENT} from '@/config/constants';
 
-export const loadMoreItems = (
+const loadMoreItems = (
     visibleItems: CodingResource[],
     setVisibleItems: (items: CodingResource[]) => void,
     filteredResources: CodingResource[]
@@ -10,3 +10,5 @@ export const loadMoreItems = (
     const moreItems = filteredResources.slice(currentLength, currentLength + LAZY_LOAD_INCREMENT);
     setVisibleItems([...visibleItems, ...moreItems]);
 };
+
+export {loadMoreItems};
