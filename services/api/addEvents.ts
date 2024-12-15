fbcf3fdfd973dd6e4b5ea4a7f4cc958537ccbbbe
DenGian <DenGian@users.noreply.github.com>
2024-12-15 23:01:43 +0100

    Refactoring

--- services/api/addEvents.ts
+++ services/api/addEvents.ts
@@ -1,6 +1,8 @@
 import {addCodingResources} from '@/services/api/addCodingResources';
 import {CodingResource} from '@/services/api/types';
 
-export const addEvents = async (newResource: CodingResource): Promise<CodingResource> => {
+const addEvents = async (newResource: CodingResource): Promise<CodingResource> => {
     return await addCodingResources(newResource);
 };
+
+export {addEvents};
