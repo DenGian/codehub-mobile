e649d6ad496f60344d3d6b18e84aec8762a5f370
DenGian <DenGian@users.noreply.github.com>
2024-12-15 23:05:29 +0100

    Refactoring

--- utils/urlUtils.ts
+++ utils/urlUtils.ts
@@ -1,6 +1,6 @@
 import {Linking} from 'react-native';
 
-export const handleOpenURL = async (url: string) => {
+const handleOpenURL = async (url: string) => {
     const supported = await Linking.canOpenURL(url);
     if (supported) {
         await Linking.openURL(url);
@@ -8,3 +8,5 @@ export const handleOpenURL = async (url: string) => {
         alert('URL not supported');
     }
 };
+
+export {handleOpenURL};
