54c4f28ec8bad226ff95843dd7761e5d11d7fa5e
DenGian <DenGian@users.noreply.github.com>
2024-12-15 23:38:38 +0100

    Imported Tos screen

--- app/(auth)/TOS.tsx
+++ app/(auth)/TOS.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import TosContent from '@/components/auth/TosContent';
+import {View} from "react-native";
+import TosScreen from "@/screens/auth/TosPage";
 
-const TermsOfService: React.FC = () => {
-    return <TosContent/>;
+const TosPage: React.FC = () => {
+    return (
+        <View style={{flex: 1}}>
+            <TosScreen/>
+        </View>
+    );
 };
 
-export default TermsOfService;
+export default TosPage;
