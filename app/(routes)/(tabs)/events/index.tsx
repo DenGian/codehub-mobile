1302fcb35c36ad3d791dff0a0b2488ad42168569
DenGian <DenGian@users.noreply.github.com>
2024-12-15 17:28:15 +0100

    Add React.FC definition for consistency and type safety

--- app/(routes)/(tabs)/events/index.tsx
+++ app/(routes)/(tabs)/events/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {View} from "react-native";
 import EventsScreen from '@/screens/route/tabs/events/EventsPage';
 
-const EventsPage = () => {
+const EventsPage: React.FC = () => {
     return (
         <View style={{flex: 1}}>
             <EventsScreen/>
