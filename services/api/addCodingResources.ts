b13b1006f286a10c1a1f8fccc284bd23f7be032e
DenGian <DenGian@users.noreply.github.com>
2026-09-14 17:22:05 +0200

    refactor: simplify resource creation service

--- services/api/addCodingResources.ts
+++ services/api/addCodingResources.ts
@@ -5,19 +5,11 @@ const addCodingResources = async (codingResource: CodingResource): Promise<Codin
     if (!codingResource) {
         throw new Error("Invalid coding resource data.");
     }
-    try {
-        const apiResponse = await apiClient<CodingResource>("/codingResources", {
-            method: "POST",
-            body: JSON.stringify(codingResource),
-        });
-        console.log("Resource added successfully:", apiResponse);
-        return apiResponse;
-    } catch (error) {
-        console.error("Error adding coding resource:", error);
-        throw error;
-    } finally {
-        console.log("Post Request completed");
-    }
+
+    return apiClient<CodingResource>("/codingResources", {
+        method: "POST",
+        body: JSON.stringify(codingResource),
+    });
 };
 
 export {addCodingResources};
