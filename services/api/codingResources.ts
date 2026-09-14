2f11901e1cf922b22ecac361ed59f11873dfd0ef
DenGian <DenGian@users.noreply.github.com>
2026-09-14 17:22:05 +0200

    refactor: simplify resource fetch service

--- services/api/codingResources.ts
+++ services/api/codingResources.ts
@@ -3,22 +3,15 @@ import {CodingResource} from "./types";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const fetchCodingResources = async (): Promise<CodingResource[]> => {
-    try {
-        const apiResources = await apiClient<CodingResource[]>("/codingResources");
+    const apiResources = await apiClient<CodingResource[]>("/codingResources");
 
-        const storedFavorites = await AsyncStorage.getItem("favorites");
-        const favorites: number[] = storedFavorites ? JSON.parse(storedFavorites) : [];
+    const storedFavorites = await AsyncStorage.getItem("favorites");
+    const favorites: number[] = storedFavorites ? JSON.parse(storedFavorites) : [];
 
-        return apiResources.map(resource => ({
-            ...resource,
-            isFavorite: favorites.includes(resource.id),
-        }));
-    } catch (error) {
-        console.error("Error fetching and marking favorites:", error);
-        throw error;
-    } finally {
-        console.log("Request completed");
-    }
+    return apiResources.map(resource => ({
+        ...resource,
+        isFavorite: favorites.includes(resource.id),
+    }));
 };
 
 export {fetchCodingResources};
