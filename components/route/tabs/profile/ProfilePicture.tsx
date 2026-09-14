1169344ff40efaf76c516975633c3d75466ca915
DenGian <DenGian@users.noreply.github.com>
2026-09-14 17:22:04 +0200

    fix: restrict profile picker to images

--- components/route/tabs/profile/ProfilePicture.tsx
+++ components/route/tabs/profile/ProfilePicture.tsx
@@ -46,7 +46,7 @@ const ProfilePicture = () => {
         }
 
         const result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ['images', 'videos'],
+            mediaTypes: ['images'],
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
